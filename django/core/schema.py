cfb34bba1ab78c672146e48df259559dc4d8ff2c
vabhi2007 <vabhi2007@gmail.com>
2025-01-27 20:28:22 -0800

    old schemas working and new account schema setup

--- django/core/schema.py
+++ django/core/schema.py
@@ -1,7 +1,10 @@
 import graphene
 import app.schema
+import account.schema
 
 class Query(app.schema.Query, graphene.ObjectType):
     pass
+class Mutation(account.schema.Mutation, app.schema.Mutation, graphene.ObjectType):
+    pass
 
-schema = graphene.Schema(query=Query)
+schema = graphene.Schema(query=Query, mutation=Mutation)
