8aa89b43937a459a98f1dafb62d8f0078c359a77
vabhi2007 <vabhi2007@gmail.com>
2025-01-29 20:16:32 -0800

    Update settings.py

--- django/core/settings.py
+++ django/core/settings.py
@@ -138,3 +138,7 @@ GRAPHENE = {
 }
 
 
+AUTHENTICATION_BACKENDS = [
+    "graphql_jwt.backends.JSONWebTokenBackend",  # Required for JWT authentication
+    "django.contrib.auth.backends.ModelBackend",
+]
