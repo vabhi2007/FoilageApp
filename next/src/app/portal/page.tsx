82997dbe21f91982715c371c10a29f49013205c1
vabhi2007 <vabhi2007@gmail.com>
2025-01-30 17:11:37 -0800

    fixed redirect

--- next/src/app/portal/page.tsx
+++ next/src/app/portal/page.tsx
@@ -28,7 +28,7 @@ export default function Portal() {
   useEffect(() => {
     console.log("User Data:", userdata);
     const token = localStorage.getItem("token");
-    if (!userloading && !userdata?.me) {
+    if (!token) {
       router.push("/signin"); // Redirect if not logged in
     }
     if (userdata?.me?.userType) {
