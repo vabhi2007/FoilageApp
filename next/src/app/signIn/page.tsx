0e3349eee6f657aa5a9353aded331088e3a09ab0
vabhi2007 <vabhi2007@gmail.com>
2025-01-30 16:12:50 -0800

    changes

--- next/src/app/signIn/page.tsx
+++ next/src/app/signIn/page.tsx
@@ -54,12 +54,12 @@ export default function Portal() {
             {/* Email */}
             <div className="my-[1.2vw] text-black">
               <div className="text-[1vw] text-gray-700">Email</div>
-              <input placeholder="Ex: johndoe@example.com" type="email" className="w-full p-[0.7vw] mt-[0.5vw] border border-gray-300 rounded-md text-[1vw]" value={username} onChange={(e) => setUsername(e.target.value)} required/>
+              <input type="email" className="w-full p-[0.7vw] mt-[0.5vw] border border-gray-300 rounded-md text-[1vw]" value={username} onChange={(e) => setUsername(e.target.value)} required/>
             </div>
             {/* Password */}
             <div className="mb-[1.2vw] text-black">
               <div className="text-[1vw] text-gray-700">Password</div>
-              <input placeholder="********" type="password" className="w-full p-[0.7vw] mt-[0.5vw] border border-gray-300 rounded-md text-[1vw]" />
+              <input type="password" className="w-full p-[0.7vw] mt-[0.5vw] border border-gray-300 rounded-md text-[1vw]" value={password} onChange={(e) => setPassword(e.target.value)} required/>
             </div>
             <div className="mb-[1.2vw]">
                 {hasAccount && (
