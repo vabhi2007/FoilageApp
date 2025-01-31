916f40ce116f3996d18e6605d531c14956bde10b
vabhi2007 <vabhi2007@gmail.com>
2025-01-31 13:06:32 -0800

    updated default isactive for job

--- django/app/models.py
+++ django/app/models.py
@@ -57,7 +57,7 @@ class JobPost(models.Model):
     grade = models.CharField(max_length=10)
     employment = models.CharField(max_length=255)
     posted_at = models.DateTimeField(auto_now_add=True)
-    is_active = models.BooleanField(default=True)
+    is_active = models.BooleanField(default=False)
 
     def __str__(self):
         return f"{self.title} at {self.company}"
